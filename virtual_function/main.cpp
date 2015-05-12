c3f9e09d55e22fe0ecaf11e2c55e1b3d0d5e358f
Zichao Zhang <zhangzichao17@gmail.com>
2015-05-12 15:14:46 +0200

    initial commit: virtual function test

---
+++ virtual_function/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "timer.h"
+
+// dynamic binding
+class Base
+{
+public:
+  virtual void doSomeThing() = 0;
+};
+
+class Derived: public Base
+{
+public:
+  virtual void doSomeThing()
+  {
+    int i = 1;
+    i++;
+  }
+};
+
+// template engine
+template<class T_engine>
+class TBase
+{
+public:
+  void doSomeThing()
+  { engine_.doSomeThing(); }
+private:
+  T_engine engine_;
+};
+
+
+int main(int argc, char *argv[])
+{
+  utils::Timer timer;
+
+  Base* dynamic_bind = new Derived();
+  TBase<Derived> static_bind;
+
+  timer.start();
+  for(int i=0; i<5000; i++)
+    dynamic_bind->doSomeThing();
+  timer.stop();
+  std::cout << "Dynamic binding: "<< timer.getMilliseconds() << std::endl;
+
+  timer.start();
+  for(int i=0; i<5000; i++)
+    static_bind.doSomeThing();
+  timer.stop();
+  std::cout << "Static binding: "<< timer.getMilliseconds() << std::endl;
+
+  return 0;
+}
